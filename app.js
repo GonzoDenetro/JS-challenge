25b1f260e27b09d3651f3dbbe4bbbf36f8945e8d
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2021-08-11 13:18:10 -0500

    Verificar datos incluido

--- app.js
+++ app.js
@@ -5,19 +5,31 @@ const disney = document.querySelector("#disney");
 const hbo = document.querySelector("#hbo");
 const compare_result = document.querySelector("#result")
 const result_value = document.querySelector("#result-value")
+const error_container = document.querySelector("#error_container")
+const close_icon = document.querySelector("#close");
+const result1 = document.querySelector("#result-1")
 
 function compare(){
     let user_movies_value = user_movies.value;
     let total_user_movies = user_movies_value * 45;
     let streaming_services_total = estados(netflix, amazon, disney, hbo);
 
-    if(total_user_movies > streaming_services_total) {
-        compare_result.innerHTML = `Te sale más barato ver tus peliculas en tus servicios de streaming.`
-        result_value.innerHTML = `Sale en $${streaming_services_total}`
+    if(user_movies_value == "" || user_movies_value == 0){
+        error_container.classList.add("error")
+        close_icon.addEventListener("click", function(){
+            error_container.classList.remove("error")
+        })
     }
     else {
-        compare_result.innerHTML = `Te sale más barato rentar tus peliculas.`
-        result_value.innerHTML = `Sale en $${total_user_movies}`
+        if(total_user_movies > streaming_services_total) {
+            compare_result.innerHTML = `Te sale más barato ver tus peliculas en tus servicios de streaming.`
+            result_value.innerHTML = `Sale en $${streaming_services_total}`
+        }
+        else {
+            compare_result.innerHTML = `Te sale más barato rentar tus peliculas.`
+            result_value.innerHTML = `Sale en $${total_user_movies}`
+        }
+        result1.innerHTML = `Rentas $${total_user_movies} VS Streaming $${streaming_services_total}`    
     }
 }
 
